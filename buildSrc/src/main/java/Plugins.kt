c78b37c4f0cec43dd9aaec3e6c58659f6dcddce1
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-04-05 15:08:16 +0900

    first commit

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,7 @@
+object Plugins {
+    const val androidApplication = "com.template.application"
+    const val androidLibrary = "com.template.library"
+    const val androidCompose = "com.template.compose"
+    const val daggerHilt = "com.template.dagger-hilt"
+    const val ktlint = "com.template.ktlint"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven(url = "https://androidx.dev/storage/compose-compiler/repository/")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven(url = "https://androidx.dev/storage/compose-compiler/repository/")
+    }
+}
+
+rootProject.name = "Template"
+include(":app")
+include(":ui")
+include(":utils")
+
+include(":lib")

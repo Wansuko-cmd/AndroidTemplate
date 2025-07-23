1959f36070520518335f512eef9beda471536b54
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-24 00:30:18 +0900

    [add] VRTを追加

--- build-logic/src/main/kotlin/AndroidComposeApplicationPlugin.kt
+++ build-logic/src/main/kotlin/AndroidComposeApplicationPlugin.kt
@@ -7,8 +7,12 @@ import ext.getPlugin
 import ext.getVersion
 import ext.implementation
 import ext.libs
+import ext.testImplementation
+import io.github.takahirom.roborazzi.RoborazziExtension
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 
@@ -19,6 +23,7 @@ class AndroidComposeApplicationPlugin : Plugin<Project> {
                 apply("org.jetbrains.kotlin.android")
                 alias(libs.getPlugin("android.application"))
                 alias(libs.getPlugin("compose.compiler"))
+                alias(libs.getPlugin("roborazzi"))
 
                 alias(libs.getPlugin("ktlint"))
             }
@@ -31,12 +36,34 @@ class AndroidComposeApplicationPlugin : Plugin<Project> {
                 buildFeatures {
                     compose = true
                 }
+
+                testOptions {
+                    unitTests {
+                        isIncludeAndroidResources = true
+                        all {
+                            it.systemProperties["robolectric.pixelCopyRenderMode"] = "hardware"
+                        }
+                    }
+                }
+            }
+
+            roborazzi {
+                generateComposePreviewRobolectricTests {
+                    enable.set(true)
+                    packages.set(listOf("com.template"))
+                    includePrivatePreviews.set(true)
+                }
             }
 
             dependencies {
                 implementation(platform(libs.getLibrary("androidx.compose.bom")))
                 implementation(libs.getBundle("androidx.compose"))
+
+                testImplementation(libs.getBundle("vrt"))
             }
         }
     }
 }
+
+private fun Project.roborazzi(configure: Action<RoborazziExtension>): Unit =
+    (this as ExtensionAware).extensions.configure("roborazzi", configure)
